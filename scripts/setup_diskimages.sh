5accb8a7e02b4ec80597f49e5f3b3228f881fa04
Keiro <admin@heimkoma.com>
2017-01-14 12:31:16 -0600

    Update setup_diskimages.sh
    Minimal checking via shellcheck.net

--- scripts/setup_diskimages.sh
+++ scripts/setup_diskimages.sh
@@ -8,7 +8,7 @@ echo -e "Images: /var/images"
 echo -e "Disks: /var/disks"
 echo -e ""
 
-read -p "Would you like to make the default directories? [y/N]:" response
+read -r -p "Would you like to make the default directories? [y/N]:" response
 
 if [[ $response =~ ^(yes|y| ) ]]; then
     mkdir /var/configs /var/images /var/disks
@@ -16,7 +16,7 @@ fi
 
 echo -e "This script can also download several ISOs to get started with your local VMs."
 echo -e " "
-read -p "Would you like to download Centos 6.6? [y/N]" response1
+read -r -p "Would you like to download Centos 6.6? [y/N]" response1
 echo -e " "
 
 if [[ $response1 =~ ^(yes|y| ) ]]; then
@@ -24,7 +24,7 @@ if [[ $response1 =~ ^(yes|y| ) ]]; then
 fi
 
 echo -e " "
-read -p "Would you like to download Centos 7.0? [y/N]" response2
+read -r -p "Would you like to download Centos 7.0? [y/N]" response2
 echo -e  " "
 
 if [[ $response2 =~ ^(yes|y| ) ]]; then
@@ -32,7 +32,7 @@ if [[ $response2 =~ ^(yes|y| ) ]]; then
 fi
 
 echo -e " "
-read -p "Would you like to download Debian 7.8? [y/N]" response3
+read -r -p "Would you like to download Debian 7.8? [y/N]" response3
 echo -e " "
 
 if [[ $response3 =~ ^(yes|y| ) ]]; then
@@ -40,11 +40,9 @@ if [[ $response3 =~ ^(yes|y| ) ]]; then
 fi
 
 echo -e " "
-read -p "Would you like to download Ubuntu 14.04? [y/N]" response4
+read -r -p "Would you like to download Ubuntu 14.04? [y/N]" response4
 echo -e " "
 
 if [[ $response4 =~ ^(yes|y| ) ]]; then
     wget https://spacepanel.io/dist/disk-images/debian-7.8.0-amd64.iso -P /var/images
 fi
-
-
