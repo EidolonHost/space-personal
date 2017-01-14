d15e2052c9ba59043fcbe79454fe77bb9a091df4
Keiro <admin@heimkoma.com>
2017-01-14 12:42:16 -0600

    Update setup_diskimages.sh
    Updated the ISO links to known good public mirrors since SpacePanel.io no longer has ISOs hosted off their sites. Besides, it's common-sense to use known good mirrors!

--- scripts/setup_diskimages.sh
+++ scripts/setup_diskimages.sh
@@ -16,33 +16,33 @@ fi
 
 echo -e "This script can also download several ISOs to get started with your local VMs."
 echo -e " "
-read -r -p "Would you like to download Centos 6.6? [y/N]" response1
+read -r -p "Would you like to download Centos 6.8? [y/N]" response1
 echo -e " "
 
 if [[ $response1 =~ ^(yes|y| ) ]]; then
-    wget https://spacepanel.io/dist/disk-images/centos-6.6-x86_64.iso -P /var/images 
+    wget http://pubmirrors.dal.corespace.com/centos/6.8/isos/x86_64/CentOS-6.8-x86_64-minimal.iso -P /var/images 
 fi
 
 echo -e " "
-read -r -p "Would you like to download Centos 7.0? [y/N]" response2
+read -r -p "Would you like to download Centos 7 Minimal? [y/N]" response2
 echo -e  " "
 
 if [[ $response2 =~ ^(yes|y| ) ]]; then
-    wget https://spacepanel.io/dist/disk-images/centos-7.0-x86_64.iso -P /var/images
+    wget http://repos.dfw.quadranet.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso -P /var/images
 fi
 
 echo -e " "
-read -r -p "Would you like to download Debian 7.8? [y/N]" response3
+read -r -p "Would you like to download Debian 8.6 Netinstall? [y/N]" response3
 echo -e " "
 
 if [[ $response3 =~ ^(yes|y| ) ]]; then
-    wget https://spacepanel.io/dist/disk-images/debian-7.8.0-amd64.iso -P /var/images
+    wget http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-cd/debian-8.6.0-amd64-netinst.iso -P /var/images
 fi
 
 echo -e " "
-read -r -p "Would you like to download Ubuntu 14.04? [y/N]" response4
+read -r -p "Would you like to download Ubuntu 16.04.1? [y/N]" response4
 echo -e " "
 
 if [[ $response4 =~ ^(yes|y| ) ]]; then
-    wget https://spacepanel.io/dist/disk-images/debian-7.8.0-amd64.iso -P /var/images
+    wget http://releases.ubuntu.com/16.04/ubuntu-16.04.1-server-amd64.iso -P /var/images
 fi
